2559becd717af52f70dc86d7ee150fc97e91c5f1
sandipdev <san.kanawade@gmail.com>
2016-03-24 11:28:14 -0400

    DB

--- sagan-common/src/main/java/sagan/DatabaseConfig.java
+++ sagan-common/src/main/java/sagan/DatabaseConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.util.Assert;
-import org.apache.commons.dbcp;
 
 public abstract class DatabaseConfig {
 
@@ -31,21 +30,23 @@ class StandaloneDatabaseConfig extends DatabaseConfig {
 
     @Bean
     public DataSource dataSource() {
-        //org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
 		
-		//dataSource.setDriverClassName("org.postgresql.Driver");
-        //dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+		dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
 		//dataSource.setUrl("jdbc:postgres://edajunppnshgkg:GnOCuBy8dmtUipKoHsDXRbqgSk@ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
 		
-		org.apache.commons.dbcp.BasicDataSource basicDataSource = new org.apache.commons.dbcp.BasicDataSource();
-		basicDataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
-        basicDataSource.setUsername("edajunppnshgkg");
-        basicDataSource.setPassword("GnOCuBy8dmtUipKoHsDXRbqgSk");
-        //dataSource.setValidationQuery("SELECT 1");
+		//dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+        dataSource.setUsername("edajunppnshgkg");
+        dataSource.setPassword("GnOCuBy8dmtUipKoHsDXRbqgSk");
+		dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
 
         //configureDataSource(dataSource);
 
-        return basicDataSource;
+        return dataSource;
     }
 }
 
