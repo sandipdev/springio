083e86d5eb3bbbe151c03db75101e8b9b0029326
sandipdev <san.kanawade@gmail.com>
2016-03-24 10:33:18 -0400

    DB URL change

--- sagan-common/src/main/java/sagan/DatabaseConfig.java
+++ sagan-common/src/main/java/sagan/DatabaseConfig.java
@@ -33,7 +33,9 @@ class StandaloneDatabaseConfig extends DatabaseConfig {
         org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
 		
 		dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+        //dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+		dataSource.setUrl("jdbc:postgres://edajunppnshgkg:GnOCuBy8dmtUipKoHsDXRbqgSk@ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+
         dataSource.setUsername("edajunppnshgkg");
         dataSource.setPassword("GnOCuBy8dmtUipKoHsDXRbqgSk");
         dataSource.setValidationQuery("SELECT 1");
