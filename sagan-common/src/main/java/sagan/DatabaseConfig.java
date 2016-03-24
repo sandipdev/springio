6958f0c1fd154fde8d36cd10b4cb925578bf23a2
sandipdev <san.kanawade@gmail.com>
2016-03-24 11:33:59 -0400

    DB

--- sagan-common/src/main/java/sagan/DatabaseConfig.java
+++ sagan-common/src/main/java/sagan/DatabaseConfig.java
@@ -61,9 +61,27 @@ class CloudFoundryDatabaseConfig extends DatabaseConfig {
 
     @Bean
     public DataSource dataSource() {
-        DataSource dataSource = cloud().getServiceConnector("sagan-db", DataSource.class, null);
-        Assert.isInstanceOf(org.apache.tomcat.jdbc.pool.DataSource.class, dataSource);
-        configureDataSource((org.apache.tomcat.jdbc.pool.DataSource) dataSource);
+        //DataSource dataSource = cloud().getServiceConnector("sagan-db", DataSource.class, null);
+        //Assert.isInstanceOf(org.apache.tomcat.jdbc.pool.DataSource.class, dataSource);
+        //configureDataSource((org.apache.tomcat.jdbc.pool.DataSource) dataSource);
+        //return dataSource;
+		
+		org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+		
+		dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+		//dataSource.setUrl("jdbc:postgres://edajunppnshgkg:GnOCuBy8dmtUipKoHsDXRbqgSk@ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+		
+		//dataSource.setUrl("jdbc:postgresql://ec2-107-22-248-209.compute-1.amazonaws.com:5432/dultlbcd1g1de");
+        dataSource.setUsername("edajunppnshgkg");
+        dataSource.setPassword("GnOCuBy8dmtUipKoHsDXRbqgSk");
+		dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+
+        //configureDataSource(dataSource);
+
         return dataSource;
     }
 }
