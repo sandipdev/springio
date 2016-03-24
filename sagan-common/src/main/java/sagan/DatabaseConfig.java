3029d837b202b2c1e54fa0e11e27dc5a052a97c5
sandipdev <san.kanawade@gmail.com>
2016-03-24 11:15:03 -0400

    DB

--- sagan-common/src/main/java/sagan/DatabaseConfig.java
+++ sagan-common/src/main/java/sagan/DatabaseConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.util.Assert;
+import org.apache.commons.dbcp;
 
 public abstract class DatabaseConfig {
 
